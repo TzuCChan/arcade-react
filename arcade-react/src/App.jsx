1e8b1372cfa96d4da7cdf218c962482f94f104e2
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-03-10 13:07:35 -0400

    const width, setWidth

--- arcade-react/src/App.jsx
+++ arcade-react/src/App.jsx
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function App() {}
+function App() {
+  const [width, setWidth] = useState('0%');
+
+  function Game() {
+    const openNav = () => {
+      setWidth('20%');
+    };
+    return (
+      <div id="icon" onClick={openNav}>
+        🎮
+      </div>
+    );
+  }
+}
 
 export default App;
